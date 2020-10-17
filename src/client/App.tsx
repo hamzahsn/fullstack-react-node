ddb245f8d01b858f8fd21fc7f9fc2fc06792c29d
hamzahsn <amdouni.hamza@hotmail.fr>
2020-10-17 12:59:02 +0200

    after format

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-
+import React from 'react'
 
 const App = () => {
+    return <div> Aloha!</div>
+}
 
-  return (
-    <div> Aloha!</div>
-  );
-};
-
-export default App;
+export default App
